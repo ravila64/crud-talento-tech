743322e7e7042c20a8d9a247fcee85148909d168
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2024-09-14 16:45:11 -0500

    arreglos en botones + mostrar fecha

--- crud.js
+++ crud.js
@@ -13,14 +13,14 @@ let currentRow; // Fila actual que se está editando
 function addMedico(nombre, email, fecha) {
   const table = document.getElementById("medicoList");
   const row = document.createElement("tr");
-
+  nombre = converterToCamelCase(nombre);
   row.innerHTML = `
         <td>${nombre}</td>
         <td>${email}</td>
         <td>${fecha}</td>
         <td class="actions">
             <button class="edit" onclick="openModal(this)">Editar</button>
-            <button class="turno" onclick="turnoMedico(this)">Turno</button>
+            <button class="turno" onclick="turnoMedico(this)">En turno</button>
         </td>
     `;
   table.appendChild(row);
@@ -62,12 +62,16 @@ function closeModal() {
 }
 
 function turnoMedico(event) {
+  const row = event.parentElement.parentElement;
   if (confirm("¿Médico termina turno?")) {
-    const row = event.parentElement.parentElement;
-    row.style.backgroundColor = "#D3D3D3"; // Cambiar el color de la fila
+    row.style.backgroundColor = "#b3c7f7"; // Cambiar el color de la fila
+    // colocar texto en boton
+  }else{
+    row.style.backgroundColor = "#f4f4f4";
   }
 }
 
+// convierte el nombre a Camel Case
 function converterToCamelCase(texto) {
   let str = "";
   let blanco = false;
@@ -93,6 +97,18 @@ function fechaHora() {
   let pos = date.indexOf("GMT");
   date = date.substring(0, pos);
   return date;
+}
+
+function displayDate() {
+  let fDateTime = fechaHora();
+  let h2Fecha = document.getElementById("fecha");
+  h2Fecha.innerHTML = fDateTime; //Date().toString();
+  h2Fecha.style.fontSize = "14px";
+  h2Fecha.style.color = "white";
+  h2Fecha.style.backgroundColor="#1c60dd";
+  h2Fecha.style.padding="12px";
+  h2Fecha.style.borderRadius="5px";
+  h2Fecha.style.collor="white";
 }
 
 // Función para validar el formato del email
